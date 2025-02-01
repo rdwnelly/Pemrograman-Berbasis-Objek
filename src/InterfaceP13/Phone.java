fd9999e671930e380181323d8c5cab8d3bbef6b3
Ridwan Elly <ridwanelly@Ridwans-MacBook-Air.local>
2025-02-01 14:20:17 +0900

    first commit

---
+++ src/InterfaceP13/Phone.java
@@ -0,0 +1,11 @@
+package InterfaceP13;
+
+public interface Phone {
+    int MAX_VOLUME = 100;
+    int MIN_VOLUME = 0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
---
+++ src/InterfaceP13/PhoneUser.java
@@ -0,0 +1,26 @@
+package InterfaceP13;
+
+public class PhoneUser {
+    
+    private Phone phone;
+
+    public PhoneUser(Phone phone) {
+        this.phone = phone;
+    }
+    
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
