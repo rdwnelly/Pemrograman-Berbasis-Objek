580287b26ec8319e7054274390e849c43fe7d997
rdwnelly <ridwanelly123@gmail.com>
2025-02-03 04:39:11 +0900

    Add files via upload

---
+++ src/AbstrakP14/Circle.java
@@ -0,0 +1,17 @@
+package AbstrakP14;
+
+public class Circle extends Shape {
+
+    private float radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+    
+    
+    @Override
+    float getArea() {
+        return (float) (Math.PI * radius * radius);
+    }
+    
+}
---
+++ src/AbstrakP14/Triangle.java
@@ -0,0 +1,18 @@
+package AbstrakP14;
+
+public class Triangle extends Shape {
+
+    private float base;
+    private float height;
+
+    public Triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    float getArea() {
+        return 0.5f * base * height;
+    }
+
+}
